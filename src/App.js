d72461a4780bcb32116f9641cbd7457d416832b0
Joseph <jsazon427@gmail.com>
2020-07-21 20:00:56 +0800

    chore: initialize redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { useEffect, Fragment } from 'react';
+import { Provider } from 'react-redux';
+
+// state
+import store from './store';
 
 // components
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -21,17 +25,19 @@ const App = () => {
   });
 
   return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
