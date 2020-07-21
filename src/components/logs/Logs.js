ef05b9ab3dfaec66267215acac283ccd00353bb5
Joseph <jsazon427@gmail.com>
2020-07-21 21:08:34 +0800

    feat: components/logs - map state to component

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,29 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+// state
+import { getLogs } from '../../actions/logActions';
 
 // components
 import LogItem from './LogItem';
 import Preloader from '../layout/Preloader';
 
-const Logs = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+const Logs = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
     // eslint-disable-next-line
   }, []);
 
-  const getLogs = async () => {
-    setLoading(true);
-
-    const res = await fetch('/logs');
-    const data = await res.json();
-
-    setLogs(data);
-    setLoading(false);
-  };
-
-  if (loading) {
+  if (loading || logs === null) {
     return <Preloader />;
   }
 
@@ -41,4 +33,10 @@ const Logs = () => {
   );
 };
 
-export default Logs;
+Logs.prototypes = { log: PropTypes.object.isRequired };
+
+const mapStateToProps = (state) => ({
+  log: state.log,
+});
+
+export default connect(mapStateToProps, { getLogs })(Logs);
